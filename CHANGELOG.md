738ccc9417173ffd5fdf9500593edecd6d722764
Jorge <72881403+JSalmon11@users.noreply.github.com>
2023-06-24 13:05:22 +0200

    [FEATURE] Verificación repeticiones caracteres

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # CHANGELOG
 
+## [1.2.0] - 2023-06-24
+### Added
+- Added verification not to repeat characters more than 2 times when generating the password.
+- Added check not to repeat consecutive identical characters.
+- Added verification to not repeat consecutive uppercase characters.
+- Added check not to repeat consecutive lowercase characters.
+- Added verification not to repeat consecutive numeric characters.
+
 ## [1.1.1] - 2022-03-12
 ### Fixed
 - Fixed button to check available updates.
@@ -16,6 +24,7 @@
 ## [1.0.0] - 2022-04-08
 - Initial release
 
+[1.2.0]: https://github.com/JSalmon11/Generador-de-Contrasenias/compare/1.1.1...1.2.0
 [1.1.1]: https://github.com/JSalmon11/Generador-de-Contrasenias/compare/1.1.0...1.1.1
 [1.1.0]: https://github.com/JSalmon11/Generador-de-Contrasenias/compare/1.0.0...1.1.0
 [1.0.0]: https://github.com/JSalmon11/Generador-de-Contrasenias/compare/b552912c04419d8d2ae5d9fd9bb7f315145f46d8...1.0.0
--- src/Utils/GeneratePass.java
+++ src/Utils/GeneratePass.java
@@ -21,9 +21,35 @@ public class GeneratePass {
         } else {
             StringBuilder sb = new StringBuilder(longitud);
             char c;
+            char lastChar = '\u0000'; //lastChar valor null
             for (int i = 0; i < sb.capacity(); ++i) {
-                c = (char) ThreadLocalRandom.current().nextInt(33, 126 + 1);
+                boolean repeat;
+                int count;
+                do {
+                    repeat = false;
+                    count = 0;
+                    c = (char) ThreadLocalRandom.current().nextInt(33, 126 + 1);
+                    for (int j = 0; j < i; j++) {
+                        if (sb.charAt(j) == c) {
+                            count++;
+                        }
+                        if (count >= 2) {
+                            repeat = true;
+                            break;
+                        }
+                    }
+                    if (c == lastChar) {
+                        repeat = true;
+                    } else if (Character.isLetter(c) && Character.isLetter(lastChar)) {
+                        if (Character.isUpperCase(c) == Character.isUpperCase(lastChar)) {
+                            repeat = true;
+                        }
+                    } else if (Character.isDigit(c) && Character.isDigit(lastChar)) {
+                        repeat = true;
+                    }
+                } while (repeat);
                 sb.insert(i, c);
+                lastChar = c;
             }
             return sb.toString();
         }
